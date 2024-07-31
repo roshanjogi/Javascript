2e470527544d87bd3b9c06455249faa3b30f7a91
roshanjogi <119349147+roshanjogi@users.noreply.github.com>
2024-07-31 09:38:27 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict";
+
+//alert(3+3)  we are using nodejs, not browser
+
+console.log(3+3);
+console.log("roshan")
+
+let name="roshan"
+let age=18
+let islogged = true
+console.log(typeof age)
